101cad20d80fe423b2c9ae473505d6fce997c63e
stephane <stephane@stephane-VirtualBox>
2016-08-09 15:36:31 +0200

    runs, except for db

--- zegveld/urls.py
+++ zegveld/urls.py
@@ -1,21 +1,43 @@
-"""zegveld URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 
-urlpatterns = [
+from filebrowser.sites import site
+from .views import HomeView, DashGroupView
+
+admin.autodiscover()
+
+urlpatterns = patterns('zegveld.views',
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^grappelli/', include('grappelli.urls')),
+    url(r'^admin/filebrowser/', include(site.urls)),
     url(r'^admin/', include(admin.site.urls)),
-]
+    url(r'^data/', include('acacia.data.urls',namespace='acacia')),
+    url(r'^(?P<name>\w+)$', DashGroupView.as_view(), name='zegveld-dashboard'),
+)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+from django.contrib.auth import views as auth_views
+urlpatterns += patterns('',
+    url(r'^password/change/$',
+                    auth_views.password_change,
+                    name='password_change'),
+    url(r'^password/change/done/$',
+                    auth_views.password_change_done,
+                    name='password_change_done'),
+    url(r'^password/reset/$',
+                    auth_views.password_reset,
+                    name='password_reset'),
+    url(r'^accounts/password/reset/done/$',
+                    auth_views.password_reset_done,
+                    name='password_reset_done'),
+    url(r'^password/reset/complete/$',
+                    auth_views.password_reset_complete,
+                    name='password_reset_complete'),
+    url(r'^password/reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$',
+                    auth_views.password_reset_confirm,
+                    name='password_reset_confirm'),
+    url(r'^accounts/', include('registration.backends.default.urls'))    
+)
