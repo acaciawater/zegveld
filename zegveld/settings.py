5cc0a1bfe3b357278f57345d6150e4e8f3f2d36c
stephane <stephane@stephane-VirtualBox>
2016-08-11 14:12:21 +0200

    adds button to zegveld_detail.html

--- zegveld/settings.py
+++ zegveld/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-os.sys.path.append('/home/theo/texelmeet/acaciadata')
+os.sys.path.append('/home/stephane/git/acaciadata')
 
 
 # Quick-start development settings - unsuitable for production
